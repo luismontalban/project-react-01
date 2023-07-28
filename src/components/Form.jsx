0c7cd71b7e589172617cc98ecba2eb3eac639414
Luis M <luis.montalban03@gmail.com>
2023-07-28 15:23:59 -0400

    Todo ok

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -4,7 +4,61 @@ export const Form = () => {
   return (
     <>
     <div className="md:w-1/2 lg:w-2/5">
-    <h2>Formulario</h2>
+    <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
+    <p className="text-lg mt-5 text-center mb-10">
+      Añade pacientes y {""}
+      <span className="text-indigo-600 font-bold">Administralos</span>
+    </p>
+
+    <form className="bg-white shadow-md rounded-lg py-10 px-5 mb-10" action="#">
+
+    <div className="mb-5">
+
+      <label htmlFor="mascota"  className="block text-gray-700 uppercase font-bold">Nombre Mascota</label>
+      <input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" id="mascota" placeholder="Nombre de la mascota" />
+    </div>
+
+
+    <div className="mb-5">
+
+<label htmlFor="propietario"  className="block text-gray-700 uppercase font-bold">Nombre Propietario</label>
+<input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="text" id="propietario" placeholder="Nombre del propietario" />
+</div>
+
+
+<div className="mb-5">
+
+<label htmlFor="email"  className="block text-gray-700 uppercase font-bold">Email</label>
+<input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="email" id="email" placeholder="Email del Propietario" />
+</div>
+
+
+<div className="mb-5">
+
+<label htmlFor="alta"  className="block text-gray-700 uppercase font-bold">Alta</label>
+<input className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" type="date" id="alta" />
+</div>
+
+
+<div className="mb-5">
+
+<label htmlFor="sintomas" className="block text-gray-700 uppercase font-bold">Síntomas</label>
+<textarea
+id="sintomas"
+className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+placeholder="Describe los Síntomas"
+
+/>
+</div>
+
+
+<input type="submit" value="Agregar Paciente" className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all" />
+
+
+    </form>
+
+
     </div>
     
     </>
---
+++ src/components/Paciente.jsx
@@ -0,0 +1,47 @@
+
+
+export const Paciente = () => {
+  return (
+    
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Nombre: {""}
+      <span className="font-normal normal-case">Wesley</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Propietario: {""}
+      <span className="font-normal normal-case">Luis Montalban</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Email: {""}
+      <span className="font-normal normal-case">luism03@gmail.com</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Fecha alta: {""}
+      <span className="font-normal normal-case">14 de Enero de 2023</span>
+    </p>
+
+    <p className="font-bold mb-3 text-gray-700 uppercase">
+      Síntomas: {""}
+      <span className="font-normal normal-case">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam
+        cumque saepe ipsum et sint. Velit libero tempora, autem odit
+        deleniti, excepturi vel animi natus hic praesentium minima
+        assumenda asperiores pariatur.
+      </span>
+    </p>
+  </div>
+
+
+
+
+
+
+
+
+
+  )
+}
